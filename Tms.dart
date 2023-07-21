09cc5db170a5f231dc33a9151dec17ecf9aa65b1
Haseeb <haseebafandi1@gmail.com>
2023-07-21 11:39:57 -0700

    Project Completed

--- Tms.dart
+++ Tms.dart
@@ -1,75 +1,146 @@
 import 'dart:io';
 
 List<Map> accounts = [
-  {"Id": 0, "Name": "admin", "Pin": 0000, "Balance": 10}
+  {"Id": 0, "Name": "admin", "Pin": 0000, "Balance": 10}, //Default data
+  {"Id": 1, "Name": "User", "Pin": 2244, "Balance": 202020} //Default data
 ];
 List Ledger = [];
 
 void main() {
-  print("======Welcome to the Transaction Management System======");
+  print("====== Welcome to the Transaction Management System ======");
 
-  bool auth = false;
-  bool exit = false;
-  int userId;
-  int Input = 0;
-  String name = 'null';
+  showATMMenu(); // Call the menu function
+}
 
-  while (!auth) {
+void showATMMenu() {
+  bool authorized = false;
+  String name;
+  int pin;
+
+  while (!authorized) {
     print("Enter your Name: ");
     name = stdin.readLineSync() ?? 'null';
 
     print("Enter your 4-digit PIN: ");
-    int pin = int.parse(stdin.readLineSync() ?? '0');
+    pin = int.parse(stdin.readLineSync() ?? '0');
 
-    auth = authorization(name, pin);
-  }
+    authorized = authorization(name, pin);
 
-  userId = getIdWithName(name);
+    while (authorized) {
+      if (getIdWithName(name) == 0) {
+        print("\n====== ATM Menu ======");
+        print("1. Create Account");
+        print("2. Check Balance");
+        print("3. Deposit Balance");
+        print("4. Withdraw Balance");
+        print("5. Delete Account");
+        print("6. Show Ledger");
+        print("7. Exit");
 
-  while (!exit) {
-    print("\n ==============MENU============== \n\n");
-    print("1.Widthdraw. \n2.Deposit \n3.Balance Inquiry \n4.Exit \n");
-    if(userId == 0){
-      print("5.Create Account \n 6.Delete Account \n");
-    }
-    Input = int.parse(stdin.readLineSync() ?? '0');
+        print("\nEnter your choice:");
+        int choice = int.parse(stdin.readLineSync() ?? '0');
 
-    if (Input == 1) {
-      print("\nEnter amount: ");
-      int amount = int.parse(stdin.readLineSync() ?? '0');
+        switch (choice) {
+          case 1:
+            String result = createAccount();
+            print("\n $result");
+            break;
 
-      print(Widthdraw(userId, amount));
-    } else if (Input == 2) {
-      print("\nEnter amount: ");
-      int amount = int.parse(stdin.readLineSync() ?? '0');
+          case 2:
+            int accountId = getIdWithName(name);
+            int balance = Check_Balance(accountId);
+            print("\nCurrent balance: $balance");
+            break;
 
-      print(Deposit(userId, amount));
-    } else if (Input == 3) {
-      print(Check_Balance(userId));
-    } else if (Input == 4) {
-      exit = true;
-    }
-    else if(userId == 0){
-      if(Input == 5){
-        print(createAccount());
+          case 3:
+            int accountId = getIdWithName(name);
+            print("\nEnter the amount to deposit:");
+            int amount = int.parse(stdin.readLineSync() ?? '0');
+            String result = Deposit(accountId, amount);
+            print("\n $result");
+            break;
+
+          case 4:
+            int accountId = getIdWithName(name);
+            print("\nEnter the amount to withdraw:");
+            int amount = int.parse(stdin.readLineSync() ?? '0');
+            String result = Widthdraw(accountId, amount);
+            print("\n $result");
+            break;
+
+          case 5:
+            print("\nEnter the account ID to delete:");
+            int accountId = int.parse(stdin.readLineSync() ?? '0');
+            String result = deleteAccount(accountId);
+            print("\n $result");
+            break;
+          
+          case 6:
+            print("\n =====Ledger======");
+
+            String data = GetLedger();
+            print(data);
+
+            break;
+
+          case 7:
+            authorized = false;
+            print(
+                "\nThank you for using the Transaction Management System Of Mustafa & Haseeb");
+            break;
+
+          default:
+            print("\nInvalid choice. Please try again. ");
+            break;
+        }
       }
-      else if(Input == 6){
+      else {
+        print("\n====== ATM Menu ======");
+        print("1. Check Balance");
+        print("2. Deposit Balance");
+        print("3. Withdraw Balance");
+        print("4. Exit");
 
-        print("\n Enter the name of the account holder: ");
-        String delName = stdin.readLineSync() ?? 'null';
-        print(deleteAccount(getIdWithName(delName)));
+        print("\nEnter your choice:");
+        int choice = int.parse(stdin.readLineSync() ?? '0');
+
+        switch (choice) {
+
+          case 1:
+            int accountId = getIdWithName(name);
+            int balance = Check_Balance(accountId);
+            print("\nCurrent balance: $balance");
+            break;
+
+          case 2:
+            int accountId = getIdWithName(name);
+            print("\nEnter the amount to deposit:");
+            int amount = int.parse(stdin.readLineSync() ?? '0');
+            String result = Deposit(accountId, amount);
+            print("\n $result");
+            break;
+
+          case 3:
+            int accountId = getIdWithName(name);
+            print("\nEnter the amount to withdraw:");
+            int amount = int.parse(stdin.readLineSync() ?? '0');
+            String result = Widthdraw(accountId, amount);
+            print("\n $result");
+            break;
+
+          case 4:
+            authorized = false;
+            print(
+                "\nThank you for using the Transaction Management System Of Mustafa & Haseeb");
+            break;
+
+          default:
+            print("\nInvalid choice. Please try again. ");
+            break;
+        }
       }
     }
-    else {
-      print("\n Invalid Input!");
-    }
   }
-
-  // createAccount();
-  // print(accounts);
-
-  // Mustafa bhai,
-  // Idhar aik menu bna dena jese aik ATM machine ka menu hota hy
 }
 
 int Check_Balance(int Id) {
@@ -103,7 +174,8 @@ String Widthdraw(int Id, int Amount) {
       "Name": accounts[Index]["Name"],
       "Old_Balance": Balance_Old,
       "Transaction Ammount": (0 - Amount),
-      "Current_Balance": Balance
+      "Current_Balance": Balance,
+      "Date": DateTime.now()
     });
 
     accounts[Index]["Balance"] = Balance;
@@ -130,7 +202,8 @@ String Deposit(int Id, int Amount) {
     "Name": accounts[Index]["Name"],
     "Old_Balance": Balance_Old,
     "Transaction Ammount": (0 + Amount),
-    "Current_Balance": Balance
+    "Current_Balance": Balance,
+    "Date": DateTime.now()
   });
 
   accounts[Index]["Balance"] = Balance;
@@ -187,4 +260,16 @@ bool authorization(String name, int pin) {
     return false;
   }
   return true;
+}
+
+String GetLedger(){
+
+  String Output = '';
+
+  for (var record in Ledger) {
+
+    Output = Output + "\n User Id: ${record['Id']}, Name: ${record['Name']}, Prevours Balance: ${record['Old_Balance']}, Transaction: ${record['Transaction Ammount']}, Current Balance: ${record['Current_Balance']}, Date: ${record['Date']}";
+  }
+
+  return Output;
 }
